8ae7a0b198a317b57725045c3b3ff69851d1d141
Piotr Krzyzak <piotrjakubkrzyzak@gmail.com>
2026-02-01 19:47:59 +0100

    Update

---
+++ dictionaries_deleting.py
@@ -0,0 +1,14 @@
+fruits = {
+    "apple": "green",
+    "orange": "orange",
+    "banana": "yellow",
+}
+
+del fruits["apple"]
+print(fruits)
+
+color = fruits.pop("banana")
+print(fruits)
+
+print(color)
+
--- ditionaries_iterating.py
+++ ditionaries_iterating.py
@@ -4,3 +4,30 @@ def main():
         "orange": "orange",
         "banana": "yellow"
     }
+
+    for fruit in fruits:
+        print(fruit + ": " + fruits[fruit])
+
+    print()
+
+    for fruit in fruits.keys():
+        print(fruit)
+
+    print()
+
+    for fruit in fruits.values():
+        print(fruit)
+
+    print("apple" in fruits)
+
+    for item in fruits.items():
+        print(item)
+
+    for item in fruits.items():
+        (fruit, color) = item
+        print(fruit + ", " + color)
+
+    for fruit, color in fruits.items():
+        print(fruit + ", " + color)
+
+main()
